a9de84acdc4984a336db6e857227037fa26b0b36
rivaz64 <59717197+rivaz64@users.noreply.github.com>
2021-09-19 13:10:44 -0500

    math mejorado

--- oaUtilities/Include/oaPlatformMath.h
+++ oaUtilities/Include/oaPlatformMath.h
@@ -9,78 +9,106 @@
 namespace oaEngineSDK {
 class OA_UTILITY_EXPORT PlatformMath {
 public:
-  static const float pi;
-  static const float radiansToDegrees;
-  static const float degreesToRadians;
+  static const float PI;
+  static const float HALF_PI;
+  static const float TWO_PI;
+  static const float INV_PI;
+  static const float INV_TWO_PI;
+  static const float RAD_TO_DEG;
+  static const float DEG_TO_RAD;
+  
+
   /**
    * @brief wraper of the cmath sqrtf
    * @param x
    * @return
   */
-  static FORCEINLINE float sqrt(float x) {
+  static FORCEINLINE float 
+  sqrt(float x) 
+  {
     return std::sqrtf(x);
   }
+
   /**
    * @brief the infamous Q_rsqrt by sir John Karmak
    * @param number
    * @return 1/sqrt(x)d
   */
-  static float invSqrt(float number);
+  static float 
+  invSqrt(float number);
+
   /**
    * @brief wraper of the cmath cos
    * @param x
    * @return
   */
-  static FORCEINLINE float cos(float x) {
+  static FORCEINLINE float 
+  cos(float x) 
+  {
     return std::cos(x);
   }
+
   /**
    * @brief wraper of the cmath sin
    * @param x
    * @return
   */
-  static FORCEINLINE float sin(float x) {
+  static FORCEINLINE float 
+  sin(float x) {
     return std::sin(x);
   }
+
   /**
    * @brief wraper of the cmath tan
    * @param x
    * @return
   */
-  static FORCEINLINE float tan(float x) {
+  static FORCEINLINE float 
+  tan(float x) {
     return std::tan(x);
   }
+
   /**
    * @brief wraper of the cmath acos
    * @param x
    * @return
   */
-  static FORCEINLINE float acos(float x) {
+  static FORCEINLINE float 
+  acos(float x) 
+  {
     return std::acos(x);
   }
+
   /**
    * @brief wraper of the cmath asin
    * @param x
    * @return
   */
-  static FORCEINLINE float asin(float x) {
+  static FORCEINLINE float 
+  asin(float x) 
+  {
     return std::asin(x);
   }
+
   /**
    * @brief wraper of the cmath atan
    * @param x
    * @return
   */
-  static FORCEINLINE float atan(float x) {
+  static FORCEINLINE float 
+  atan(float x) 
+  {
     return std::atan(x);
   }
+
   /**
    * @brief wraper of the cmath atan2
    * @param x
    * @param y
    * @return
   */
-  static FORCEINLINE float atan2(float x, float y) {
+  static FORCEINLINE float 
+  atan2(float x, float y) {
     return std::atan2(y, x);
   }
 
@@ -91,7 +119,9 @@ public:
    * @return 
   */
   template<class T>
-  static FORCEINLINE T abs(T v) {
+  static FORCEINLINE T 
+  abs(T v) 
+  {
     return std::abs(v);
   }
 
@@ -103,7 +133,9 @@ public:
    * @return
   */
   template<class T>
-  static FORCEINLINE T max(T v1,T v2) {
+  static FORCEINLINE T 
+  max(T v1,T v2) 
+  {
     return std::max(v1,v2);
   }
 
@@ -115,8 +147,88 @@ public:
    * @return
   */
   template<class T>
-  static FORCEINLINE T min(T v1, T v2) {
+  static FORCEINLINE T 
+  min(T v1, T v2) 
+  {
     return std::min(v1, v2);
+  }
+
+  /**
+   * @brief wraper for the std::exp
+   * @tparam T
+   * @param v
+  */
+  template<class T>
+  static FORCEINLINE T 
+  exp(T v) {
+    return std::exp(v1, v2);
+  }
+
+  /**
+   * @brief wraper of the std::fmod
+   * @tparam T 
+   * @param v1 
+   * @param v2 
+  */
+  template<class T>
+  static FORCEINLINE T
+  mod(T v1, T v2)
+  {
+    return std::fmod(v1, v2);
+  }
+
+  /**
+   * @brief wraper of the std::pow
+   * @tparam T 
+   * @param v1 
+   * @param v2 
+   * @return 
+  */
+  template<class T>
+  static FORCEINLINE T
+  pow(T v1, T v2)
+  {
+    return std::pow(v1, v2);
+  }
+
+  /**
+   * @brief wraper of the std::log
+  */
+  template<class T>
+  static FORCEINLINE T
+  log(T v)
+  {
+    return std::log(v1);
+  }
+
+  /**
+   * @brief 1 if positive, -1 if negative, 0 if 0
+   * @tparam T 
+   * @param val 
+   * @return 
+  */
+  template <typename T>
+  static FORCEINLINE int
+  sign(T v) {
+    return (T(0) < v) - (v < T(0));
+  }
+
+  /**
+   * @brief a random number between 0 and 1
+   * @return 
+  */
+  static FORCEINLINE float
+  unitRandom() {
+    return (float)std::rand() / RAND_MAX;
+  }
+
+  /**
+   * @brief a random number between 0 and 1
+   * @return
+  */
+  static FORCEINLINE float
+  symmetricRandom() {
+    return ((float)std::rand() / RAND_MAX)*2.f-1.f;
   }
 };
 
--- oaUtilities/Source/oaPlatformMath.cpp
+++ oaUtilities/Source/oaPlatformMath.cpp
@@ -1,7 +1,11 @@
 #include "oaPlatformMath.h"
-const float oaEngineSDK::PlatformMath::pi = 3.1415926535897932384626433832795f;
-const float oaEngineSDK::PlatformMath::radiansToDegrees = 180.f / pi;
-const float oaEngineSDK::PlatformMath::degreesToRadians = pi / 180.f;
+const float oaEngineSDK::PlatformMath::PI = 3.1415926535897932384626433832795f;
+const float oaEngineSDK::PlatformMath::HALF_PI = 1.57079632679489661923132169163975f;
+const float oaEngineSDK::PlatformMath::TWO_PI = 6.283185307179586476925286766559f;
+const float oaEngineSDK::PlatformMath::INV_PI = 0.318309886183790671537767526745f;
+const float oaEngineSDK::PlatformMath::INV_TWO_PI = 0.159154943091895335768883763372f;
+const float oaEngineSDK::PlatformMath::RAD_TO_DEG = 57.2957795130823208767981548141f;
+const float oaEngineSDK::PlatformMath::DEG_TO_RAD = 0.01745329251994329576923690768f;
 float OA_UTILITY_EXPORT oaEngineSDK::PlatformMath::invSqrt(float number)
 {
   long i;
