01e24f6a60ad8eec760c99081a18a880487dfda8
rivaz64 <59717197+rivaz64@users.noreply.github.com>
2021-12-01 13:42:43 -0600

    Update oaDX11IndexBuffer.cpp

--- oaDX11Graphics/source/oaDX11IndexBuffer.cpp
+++ oaDX11Graphics/source/oaDX11IndexBuffer.cpp
@@ -6,8 +6,8 @@ bool oaEngineSDK::DX11IndexBuffer::init(Vector<uint32>& data)
   D3D11_BUFFER_DESC bd;
   ZeroMemory( &bd, sizeof(bd) );
   bd.Usage = D3D11_USAGE_DEFAULT;
-  bd.ByteWidth = sizeof(uint32);
-  bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+  bd.ByteWidth = sizeof(uint32)*data.size();
+  bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
   bd.CPUAccessFlags = 0;
   
   D3D11_SUBRESOURCE_DATA InitData;
