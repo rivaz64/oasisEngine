08292f9b7709f615e767a95c003876772d4344f5
rivaz64 <59717197+rivaz64@users.noreply.github.com>
2022-02-04 20:55:36 -0600

    color created

--- oaUtilities/Include/oaColor.h
+++ oaUtilities/Include/oaColor.h
@@ -12,6 +12,37 @@ namespace oaEngineSDK{
 
 class Color
 {
+ public:
+  /**
+   * @brief default constuctor for the color
+  */
+  Color() = default;
+
+  /**
+   * @brief parametrized constructor
+   * @param _r red
+   * @param _g green
+   * @param _b blue
+   * @param _a alpha
+  */
+  Color(float _r, float _g, float _b, float _a = 1.0f) :
+    r(_r), g(_g), b(_b), a(_a) {}
+
+  static const Color BLACK;
+  static const Color WHITE;
+  static const Color RED;
+  static const Color GREEN;
+  static const Color BLUE;
+  static const Color YELLOW;
+  static const Color MAGENTA;
+  static const Color CYAN;
+  static const Color ORANGE;
+  static const Color RASPBERRY;
+  static const Color VIOLET;
+  static const Color OCEAN;
+  static const Color TURQUOISE;
+  static const Color SPRINGGREEN;
+
   /**
    * @brief red
   */
--- oaUtilities/Source/oaColor.cpp
+++ oaUtilities/Source/oaColor.cpp
@@ -1,1 +1,20 @@
 #include "oaColor.h"
+
+namespace oaEngineSDK{
+
+static const Color BLACK(0.f,0.f,0.f,1.f);
+static const Color WHITE(1.f,1.f,1.f,1.f);
+static const Color RED(1.f,0.f,0.f,1.f);
+static const Color GREEN(0.f,1.f,0.f,1.f);
+static const Color BLUE(0.f,0.f,1.f,1.f);
+static const Color YELLOW(1.f,1.f,0.f,1.f);
+static const Color MAGENTA(1.f,0.f,1.f,1.f);
+static const Color CYAN(0.f,1.f,1.f,1.f);
+static const Color ORANGE(1.f,0.5f,0.f,1.f);
+static const Color RASPBERRY(1.f,0.f,0.5f,1.f);
+static const Color VIOLET(0.5f,0.f,1.f,1.f);
+static const Color OCEAN(0.f,0.5f,1.f,1.f);
+static const Color TURQUOISE(0.f,1.f,0.5f,1.f);
+static const Color SPRINGGREEN(0.5f,1.f,0.f,1.f);
+
+}
